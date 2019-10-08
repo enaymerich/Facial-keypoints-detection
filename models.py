1cdc1d97ef1fcf89f2be7af0509bbf9936ae18f2
enaymerich <41734507+enaymerich@users.noreply.github.com>
2019-10-08 15:03:56 +0200

    Model and notebook 2

--- models.py
+++ models.py
@@ -17,10 +17,25 @@ class Net(nn.Module):
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+        ##INPUT IMAGE SIZE: 120x120
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
+        ##out: (120-Fsize)/Stride + 1 = 116x116
+        # maxpool layer
+        # pool with kernel_size=2, stride=2
+        self.pool = nn.MaxPool2d(8, 8)
+        ##out: 116/4x116/4
+
+        # 20 outputs * the 5*5 filtered/pooled map size
+        self.fc1 = nn.Linear(32 * 14 * 14, 1000)
+        self.fc2 = nn.Linear(1000, 136)
+
+        # dropout with p=0.4
+        self.fc1_drop = nn.Dropout(p=0.4)
+        self.fc2_drop = nn.Dropout(p=0.4)
+
+
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -31,7 +46,17 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = self.pool(F.relu(self.conv1(x)))
+        # prep for linear layer
+        # this line of code is the equivalent of Flatten in Keras
+        x = x.view(x.size(0), -1)
+
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+net = Net()
+print(net)
