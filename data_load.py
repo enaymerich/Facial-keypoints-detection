5b1c533777fc53405369514a15f5867e4bc20e38
Enrico Aymerich <enrico.aymerich@ipp.mpg.de>
2020-02-22 09:31:38 +0100

    First commit of the project

--- data_load.py
+++ data_load.py
@@ -49,6 +49,12 @@ class FacialKeypointsDataset(Dataset):
     
 # tranforms
 
+import torch
+from torchvision import transforms, utils
+import random
+import PIL
+# tranforms
+
 class Normalize(object):
     """Convert a color image to grayscale and normalize the color range to [0,1]."""        
 
@@ -63,12 +69,10 @@ class Normalize(object):
         
         # scale color range from [0, 255] to [0, 1]
         image_copy=  image_copy/255.0
-            
         
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
-        key_pts_copy = (key_pts_copy - 100)/50.0
-
+        key_pts_copy = (key_pts_copy - 48)/image_copy.shape[1]*2
 
         return {'image': image_copy, 'keypoints': key_pts_copy}
 
@@ -132,12 +136,13 @@ class RandomCrop(object):
 
         top = np.random.randint(0, h - new_h)
         left = np.random.randint(0, w - new_w)
+        key_pts = key_pts - [left, top]
 
         image = image[top: top + new_h,
                       left: left + new_w]
 
-        key_pts = key_pts - [left, top]
-
+        h, w = np.min(key_pts,axis=0)
+        
         return {'image': image, 'keypoints': key_pts}
 
 
@@ -156,6 +161,58 @@ class ToTensor(object):
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
-        
         return {'image': torch.from_numpy(image),
                 'keypoints': torch.from_numpy(key_pts)}
+
+class RandomHorizontalFlip(object):
+    """Horizontally flip the given PIL Image randomly with a given probability.
+
+    Args:
+        p (float): probability of the image being flipped. Default value is 0.5
+    """
+
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, sample):
+        """
+        Args:
+            img (PIL Image): Image to be flipped.
+
+        Returns:
+            PIL Image: Randomly flipped image.
+        """
+        if random.random() < self.p:
+            new_keypoints = abs(sample['keypoints']-np.array([sample['image'].shape[1],0]))
+            new_image = PIL.Image.fromarray(sample['image'])
+            new_image = np.array(new_image.transpose(Image.FLIP_LEFT_RIGHT))
+            return {'image': new_image,
+                'keypoints': new_keypoints}
+        return {'image': sample['image'],
+                'keypoints': sample['keypoints']}
+class RandomVerticalFlip(object):
+    """Horizontally flip the given PIL Image randomly with a given probability.
+
+    Args:
+        p (float): probability of the image being flipped. Default value is 0.5
+    """
+
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, sample):
+        """
+        Args:
+            img (PIL Image): Image to be flipped.
+
+        Returns:
+            PIL Image: Randomly flipped image.
+        """
+        if random.random() < self.p:
+            new_keypoints = abs(sample['keypoints']-np.array(0,[sample['image'].shape[0]]))
+            new_image = PIL.Image.fromarray(sample['image'])
+            new_image = np.array(new_image.transpose(Image.FLIP_TOP_BOTTOM))
+            return {'image': new_image,
+                'keypoints': new_keypoints}
+        return {'image': sample['image'],
+                'keypoints': sample['keypoints']}
--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -17,46 +16,73 @@ class Net(nn.Module):
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        ##INPUT IMAGE SIZE: 120x120
+        
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
-        ##out: (120-Fsize)/Stride + 1 = 116x116
-        # maxpool layer
-        # pool with kernel_size=2, stride=2
-        self.pool = nn.MaxPool2d(8, 8)
-        ##out: 116/4x116/4
-
-        # 20 outputs * the 5*5 filtered/pooled map size
-        self.fc1 = nn.Linear(32 * 14 * 14, 1000)
-        self.fc2 = nn.Linear(1000, 136)
-
-        # dropout with p=0.4
-        self.fc1_drop = nn.Dropout(p=0.4)
-        self.fc2_drop = nn.Dropout(p=0.4)
-
-
+        ##### 224 x 224 input images
+        # output size = (W-F)/S +1 = (224 - 5)/1 + 1 = 220
+        # the output Tensor for one image, will have the dimensions: (32, 220, 220)
+        # after one pool layer, this becomes (32, 110, 110)
+        
+        # output size = (W-F)/S +1 = (110-4)/1 +1 = 107
+        # the output Tensor for one image, will have the dimensions: (64, 107, 107)
+        # after one pool layer, this becomes (64, 53, 53)
+        self.conv2 = nn.Conv2d(32, 64, 4)
+        
+        # output size = (W-F)/S +1 = (53-3)/1 +1 = 51
+        # the output Tensor for one image, will have the dimensions: (128, 51, 51)
+        # after one pool layer, this becomes (128, 25, 25) - rounds down
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        
+        # output size = (W-F)/S +1 = (25-2)/1 +1 = 24
+        # the output Tensor for one image, will have the dimensions: (256, 24, 24)
+        # after one pool layer, this becomes (256, 12, 12)
+        self.conv4 = nn.Conv2d(128, 256, 2)
+        
         
         ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers 
+        # (such as dropout or batch normalization) to avoid overfitting
         
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.dropout1 = nn.Dropout(p=0.1)
+        self.dropout2 = nn.Dropout(p=0.2)
+        self.dropout3 = nn.Dropout(p=0.3)
+        self.dropout4 = nn.Dropout(p=0.4)
+        self.dropout5 = nn.Dropout(p=0.5)
+        self.dropout6 = nn.Dropout(p=0.6)
+        
+        # 256 outputs * the 12*12 filtered/pooled map size = 36864
+        self.fc1 = nn.Linear(256*12*12, 1000)
+        
+        self.fc2 = nn.Linear(1000, 1000)
+        
+        self.fc3 = nn.Linear(1000, 136)
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        
         x = self.pool(F.relu(self.conv1(x)))
-        # prep for linear layer
-        # this line of code is the equivalent of Flatten in Keras
+        x = self.dropout1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.dropout3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.dropout4(x)
+        
+        # Flatten feature maps into a vector
         x = x.view(x.size(0), -1)
-
-        # two linear layers with dropout in between
+        
         x = F.relu(self.fc1(x))
-        x = self.fc1_drop(x)
-        x = self.fc2(x)
-
+        #x = self.dropout5(x)
+        x = F.relu(self.fc2(x))
+        #x = self.dropout6(x)
+        x = self.fc3(x)
+        
         # a modified x, having gone through all the layers of your model, should be returned
         return x
-net = Net()
-print(net)
